c768113a535b9f36dea7bf8730a24c5e4c789703
Rózsavölgyi Botond <Rzsavlgyi Botond@DESKTOP-780AENH>
2018-05-28 09:04:31 +0200

    20180528 - final

--- esper_debs2015_projekt/src/test/java/hu/bme/mit/test/esper/EsperTask2Test.java
+++ esper_debs2015_projekt/src/test/java/hu/bme/mit/test/esper/EsperTask2Test.java
@@ -1,31 +1,16 @@
 package hu.bme.mit.test.esper;
 
-import static org.junit.Assert.*;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
-
-import org.junit.After;
-//import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.espertech.esper.client.EPOnDemandPreparedQueryParameterized;
 import com.espertech.esper.client.EPRuntime;
-import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.time.CurrentTimeEvent;
 
-import hu.bme.mit.entities.AreaWithProfit;
 import hu.bme.mit.entities.TaxiLog;
 import hu.bme.mit.esper.main.App;
-import hu.bme.mit.positioning.Cell;
 import hu.bme.mit.test.AbstractTask2Test;
-import hu.bme.mit.toplist.ProfitableAreaToplistSet;
 
 public class EsperTask2Test extends AbstractTask2Test{
 
